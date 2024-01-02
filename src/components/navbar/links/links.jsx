515a6100f636add8749748ff109609f420c712e5
haikaljs <haikaljavascript@gmail.com>
2024-01-02 15:28:47 +0800

    Active navbar link class

--- src/components/navbar/links/links.jsx
+++ src/components/navbar/links/links.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import styles from "./links.module.css";
+import NavLink from "./navLink/navLink";
 
 export const Links = () => {
   const links = [
@@ -20,11 +21,24 @@ export const Links = () => {
       path: "/blog",
     },
   ];
+
+  // TEMPORARY
+  const session = true;
+  const isAdmin = true;
+
   return (
     <div className={styles.links}>
       {links.map((link) => (
-        <Link href={link.path}>{link.title}</Link>
+        <NavLink item={link} key={link.title} />
       ))}
+      {session ? (
+        <>
+          {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}{" "}
+          <button className={styles.logout}>Logout</button>
+        </>
+      ) : (
+        <NavLink item={{ title: "Login", path: "/login" }} />
+      )}
     </div>
   );
 };
